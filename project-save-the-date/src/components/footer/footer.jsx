bfb1ecd7b73dee8851adbc326c0039e58ffae193
Rebekah <rebekahcodes@gmail.com>
2024-03-29 16:28:09 +0000

    added header

---
+++ project-save-the-date/src/components/footer/footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './footer.css'
+
+export default function Footer(props) {
+  return (
+
+<header className="site-footer">
+     <h1>🔥Save The Date Footer</h1>
+</header>
+
+
+  );
+}
---
+++ project-save-the-date/src/components/header/header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './header.css'
+
+export default function Header(props) {
+  return (
+
+<header className="site-header">
+     <h1 class="site-title">Save The Date</h1>
+</header>
+
+
+  );
+}
