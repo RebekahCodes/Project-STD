04257508e028f14a6043c565dc5220b6239ae84c
Rebekah <rebekahcodes@gmail.com>
2024-03-29 23:44:22 +0000

    fixed responsive layout

--- project-save-the-date/app/page.js
+++ project-save-the-date/app/page.js
@@ -9,13 +9,15 @@ export default function Home() {
     <>
       <div className="home-container">
         <HeroImage src="/images/hero-image.png" alt="engagement photo" />
-        <Info
-          title="Becky & Gerrone Are Getting Married!"
-          date="4th July 2025"
-          location="SURREY, UK"
-          text="Please click below to enter your contact details so that we can send your invitation"
-        />
-        <Button label="Enter Your Contact Details" />
+        <div className="info-button">
+          <Info
+            title="Becky & Gerrone Are Getting Married!"
+            date="4th July 2025"
+            location="SURREY, UK"
+            text="Please click below to enter your contact details so that we can send your invitation"
+          />
+          <Button label="Enter Your Contact Details" />
+        </div>
       </div>
     </>
   );
