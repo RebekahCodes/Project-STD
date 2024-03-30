3b75c6e9eb515b650382f99bfaf8839eb3ecdf49
Rebekah <rebekahcodes@gmail.com>
2024-03-30 11:01:02 +0000

    added link to sign-up page

--- project-save-the-date/app/page.js
+++ project-save-the-date/app/page.js
@@ -2,22 +2,27 @@ import "./page.css";
 import HeroImage from "../src/components/hero-image/hero-image.jsx";
 import Info from "../src/components/info/info.jsx";
 import Button from "../src/components/button/button.jsx";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
-      <div className="home-container">
-        <HeroImage src="/images/hero-image.png" alt="engagement photo" />
-        <div className="info-grid">
-          <Info
-            title="Becky & Gerrone Are Getting Married!"
-            date="4th July 2025"
-            location="SURREY, UK"
-            text="Please click below to enter your contact details so that we can send your invitation"
-          />
-          <Button label="Get Your Invite" />
+    <div className="home-container">
+      <HeroImage src="/images/hero-image.png" alt="engagement photo" />
+
+      <div className="info-grid">
+        <Info
+          title="Becky & Gerrone Are Getting Married!"
+          date="4th July 2025"
+          location="SURREY, UK"
+          text="Please click below to enter your contact details so that we can send your invitation"
+        />
+
+        <div className="button-link">
+          <Link href="/sign-up">
+            <Button label="Get Your Invite" />
+          </Link>
         </div>
       </div>
-    </>
+    </div>
   );
 }
