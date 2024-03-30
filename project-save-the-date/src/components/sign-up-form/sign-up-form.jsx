c373e5a4387437d18901f9d8e27d5021d005ae76
Rebekah <rebekahcodes@gmail.com>
2024-03-30 14:50:04 +0000

    added reducer state updaters for last name and email inputs

--- project-save-the-date/src/components/sign-up-form/sign-up-form.jsx
+++ project-save-the-date/src/components/sign-up-form/sign-up-form.jsx
@@ -48,10 +48,11 @@ function reducer(state, action) {
 }
 
 export default function SignUpForm() { //create a sign up form component
+  
   const [state, dispatch] = useReducer(reducer, blankForm); //pass blankForm state to the reducer function
 
   function handleInputChanges(event, index) {
-    //create a function to update the input fields as the user types
+    //create a function to update the input field values as the user types (onChange)
     if (event.target.name === "firstName") {
       //if the input fields name is "firstName"
       dispatch({
@@ -64,15 +65,48 @@ export default function SignUpForm() { //create a sign up form component
         },
       });
       console.log("First Name:", event.target.value);
-      console.log(index)
+      console.log("index is:", index)
     }
+
+    if (event.target.name === "lastName") {
+      //if the input fields name is "lastName"
+      dispatch({
+        type: "UPDATE_GUEST_DATA", //dispatch this action to the reducer
+        payload: {
+          //payload identifies the data we want to be updated in the new object(state)
+          index: index, //index will be whichever number were at in the guestData array.
+          name: event.target.name, //event.target.name will be "lastName".
+          value: event.target.value, // event.target.value will be whatever name the user types.
+        },
+      });
+      console.log("Last Name:", event.target.value);
+      console.log("index is:", index)
+    }
+
+    if (event.target.name === "email") {
+      //if the input fields name is "email"
+      dispatch({
+        type: "UPDATE_GUEST_DATA", //dispatch this action to the reducer
+        payload: {
+          //payload identifies the data we want to be updated in the new object(state)
+          index: index, //index will be whichever number were at in the guestData array.
+          name: event.target.name, //event.target.name will be "email".
+          value: event.target.value, // event.target.value will be whatever name the user types.
+        },
+      });
+      console.log("email:", event.target.value);
+      console.log("index is:", index)
+    }
+
+
   }
 
   return (
     <div className="signup-form-container">
       {state.guestData.map((guest,index) => { //for the current state, loop through each guest in guestData array. index here just signifies the guest you are on.
-        return (
-          <form key={index}>
+        
+        return (//form needs to have a unique key in react, give the index as the key.
+          <form key={index}> 
             <label htmlFor="firstName">first name</label>
             <input
               id="firstName"
@@ -83,10 +117,20 @@ export default function SignUpForm() { //create a sign up form component
             />
 
             <label htmlFor="lasttName">last name</label>
-            <input id="lastName" type="text" name="lastName" />
+            <input id="lastName" 
+            type="text" 
+            name="lastName" 
+            value={guest.lastName}
+            onChange={event => (handleInputChanges(event, index))}
+            />
 
             <label htmlFor="email">email</label>
-            <input id="email" type="email" name="email" />
+            <input id="email" 
+            type="email" 
+            name="email"
+            value={guest.email}
+            onChange={event => (handleInputChanges(event, index))}
+            required/>
           </form>
         );
       })}
