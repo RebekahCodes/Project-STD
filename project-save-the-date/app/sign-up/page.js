830bc7dc6dae58e8363783434490ec1c6dfa6863
Rebekah Anderson <rebekahanderson@Rebekahs-MacBook-Air.local>
2024-06-26 17:53:45 +0100

    testing dev deploment

--- project-save-the-date/app/sign-up/page.js
+++ project-save-the-date/app/sign-up/page.js
@@ -8,8 +8,8 @@ export default function SignUp() {
       <div className="sign-up-page-container">
         <Info
           title="Becky & Gerrone"
-          date="4th July 2024"
-          text="Dev-Test If more than one person from your household has received a save-the-date, you can respond on their behalf by choosing to 'Add A Guest'"
+          date="4th June 2024"
+          text="If more than one person from your household has received a save-the-date, you can respond on their behalf by choosing to 'Add A Guest'"
         />
         <SignUpForm />
       </div>
