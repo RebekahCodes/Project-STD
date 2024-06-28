f0ea963540daec48a95ac48b469b4eb12fcfb1a9
Rebekah Anderson <rebekahanderson@Rebekahs-MacBook-Air.local>
2024-06-28 16:50:27 +0100

    added coupld names variable

--- project-save-the-date/app/sign-up/page.js
+++ project-save-the-date/app/sign-up/page.js
@@ -8,7 +8,7 @@ export default function SignUp() {
       <div className="sign-up-page-container">
         <Info
           title="Becky & Gerrone"
-          date="4th July 2025"
+          date={Process.env.NEXT_PUBLIC_DATE}
           text="If more than one person from your household has received a save-the-date, you can respond on their behalf by choosing to 'Add A Guest'"
         />
         <SignUpForm />
