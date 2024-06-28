c61e96d45c5ae658e7f8a08929fb68ea25eae4e7
Rebekah Anderson <rebekahanderson@Rebekahs-MacBook-Air.local>
2024-06-28 22:44:45 +0100

    added thank you details

--- project-save-the-date/app/thank-you-faq/page.js
+++ project-save-the-date/app/thank-you-faq/page.js
@@ -5,16 +5,18 @@ import Info from "@/components/info/info";
 export default function ThankYouFaq() {
   return (
     <div className="home-container">
-      <HeroImage src={process.env.NEXT_PUBLIC_IMAGE_URL} alt="photo of a couple" />
+      
 
       <div className="info-grid">
         <Info
-          title="Thank You for Signing Up!"
-          date={process.env.NEXT_PUBLIC_DATE}
-          location={process.env.NEXT_PUBLIC_LOCATION}
-          text="We have received your information and will be in touch soon."
+        title="Thank You!"
+           //date={process.env.NEXT_PUBLIC_DATE}
+           location="We're excited to celebrate our special day with you and have chosen to host an intimate adult-only wedding."
+          
+          text="Details to follow."
         />
       </div>
+      <HeroImage src={process.env.NEXT_PUBLIC_IMAGE_URL} alt="photo of a couple" />
     </div>
   );
 }
