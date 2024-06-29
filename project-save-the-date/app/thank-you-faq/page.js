705e2baa88646f23e11e1b34258534119716058c
Rebekah Anderson <rebekahanderson@Rebekahs-MacBook-Air.local>
2024-06-29 13:46:37 +0100

    added add to calendar button

--- project-save-the-date/app/thank-you-faq/page.js
+++ project-save-the-date/app/thank-you-faq/page.js
@@ -1,3 +1,6 @@
+"use client";
+import React from "react";
+import { AddToCalendarButton } from 'add-to-calendar-button-react';
 import "./thank-you-faq.css";
 import HeroImage from "@/components/hero-image/hero-image";
 import Info from "@/components/info/info";
@@ -12,9 +15,25 @@ export default function ThankYouFaq() {
         title="Thank You!"
            //date={process.env.NEXT_PUBLIC_DATE}
            location="We have received your contact details and will be in touch soon."
+           text={process.env.NEXT_PUBLIC_DATE}
         />
       </div>
+      
+      <div className="add-to-calendar-button">
+      
+      <AddToCalendarButton
+  name={process.env.NEXT_PUBLIC_TITLE}
+  options={['Apple', 'Google', 'iCal', 'Microsoft365', 'Outlook.com', 'Yahoo']}
+  location={process.env.NEXT_PUBLIC_LOCATION}
+  startDate="2025-07-04"
+  endDate="2025-07-04"
+  timeZone="Europe/London"
+  label="Add To My Calendar"
+/>
+</div>
+<div/>
       <HeroImage src={process.env.NEXT_PUBLIC_IMAGE_URL} alt="photo of a couple" />
+    
     </div>
   );
 }
