97bbe0e3f6ca2826e22b251061c443df469439ce
Rebekah Anderson <rebekahanderson@Rebekahs-MacBook-Air.local>
2024-06-29 12:22:00 +0100

    swapped text content

--- project-save-the-date/app/thank-you-faq/page.js
+++ project-save-the-date/app/thank-you-faq/page.js
@@ -11,9 +11,7 @@ export default function ThankYouFaq() {
         <Info
         title="Thank You!"
            //date={process.env.NEXT_PUBLIC_DATE}
-           location="We're excited to share our special day with you and have chosen to host an intimate adult-only celebration."
-          
-          text="Details to follow."
+           location="We have received your contact details and will be in touch soon."
         />
       </div>
       <HeroImage src={process.env.NEXT_PUBLIC_IMAGE_URL} alt="photo of a couple" />
